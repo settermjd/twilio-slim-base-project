da0e6e0741991eb6ec2b7eeb9fe7b973169ffe89
Matthew Setter <matthew@matthewsetter.com>
2026-01-30 15:24:02 +1000

    Update the bootstrap file
    This change ensures that the minimum three environment variables
    required to make authenticated requests to Twilio's APIs are available,
    and that a Twilio Rest Client is registered with the application's
    DI container.

--- public/index.php
+++ public/index.php
@@ -5,12 +5,32 @@ declare(strict_types=1);
 use App\Application;
 use DI\Container;
 use Dotenv\Dotenv;
+use Twilio\Rest\Client;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Load environment variables from .env in the project's parent directory
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-$application = new Application(new Container());
+// Ensure that the following environment variables are available and not empty.
+// They are available in the Twilio Console (https://console.twilio.com) in the Account Info panel.
+$dotenv->required([
+    'TWILIO_ACCOUNT_SID',
+    'TWILIO_AUTH_TOKEN',
+    'TWILIO_PHONE_NUMBER',
+])->notEmpty();
+
+$container = new Container();
+
+/**
+ * Register a Twilio REST Client object as a service with the DI container
+ */
+$container->set(
+    Client::class,
+    fn() => new Client($_ENV['TWILIO_ACCOUNT_SID'], $_ENV['TWILIO_AUTH_TOKEN']),
+);
+
+$application = new Application($container);
 $application->setupRoutes();
 $application->run();
